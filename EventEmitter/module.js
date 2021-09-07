ba05dcb5ecd3596beb9adef08e832599d746f61f
WocketSesh <jackcommon1@outlook.com>
2021-09-07 09:32:42 +0100

    Fix

--- EventEmitter/module.js
+++ EventEmitter/module.js
@@ -5,14 +5,15 @@ class Events {
 
 	on(event, func) {
 		this.event = event;
-		if (typeof func != "function") return;
+		if (typeof func != "function")
+			throw new TypeError("Needs to be of type function.");
 		this.func = func;
 
 		a.push({ event: event, name: this.name, func: this.func });
 	}
 
 	emit(event) {
-		let e = [...a].filter((x) => x.name == this.name && x.event == event);
+		let e = a.filter((x) => x.name == this.name && x.event == event);
 		if (!e[0]) return;
 
 		e[0].func(...[...arguments].splice(1));
