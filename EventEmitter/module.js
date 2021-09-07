c6eab5d5b7868e0b50060c7e524e023f71b22611
WocketSesh <jackcommon1@outlook.com>
2021-09-07 13:57:54 +0100

    clean up event emitter

--- EventEmitter/module.js
+++ EventEmitter/module.js
@@ -1,22 +1,14 @@
-var a = [];
-
 class Events {
 	constructor() {}
 
 	on(event, func) {
-		this.event = event;
 		if (typeof func != "function")
 			throw new TypeError("Needs to be of type function.");
-		this.func = func;
-
-		a.push({ event: event, name: this.name, func: this.func });
+		this[event] = func;
 	}
 
 	emit(event) {
-		let e = a.filter((x) => x.name == this.name && x.event == event);
-		if (!e[0]) return;
-
-		e[0].func(...[...arguments].splice(1));
+		if (this[event]) this[event](...[...arguments].splice(1));
 	}
 }
 
